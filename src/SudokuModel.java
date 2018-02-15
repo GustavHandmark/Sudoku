d3a7c092d3afd6354d9c0b0edb2666cc29cb6db5
chokladio <chokladio@gmail.com>
2018-02-15 16:09:24 +0100

    sry

--- src/SudokuModel.java
+++ src/SudokuModel.java
@@ -6,15 +6,6 @@ public class SudokuModel {
     public static void main(String[] args){
 		System.out.println("sudoku");
 	}
-    hej jag heter claudio
-    test teste
-    
-    
-    aj aj!
-    
-    hmmmm;
-    
-	
-	
+
 	
 }
