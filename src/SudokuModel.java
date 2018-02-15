17519142eda5d43378c0b5b87241dbec37ebc1d0
Gustav <none>
2018-02-15 15:05:25 +0100

    changes

--- src/SudokuModel.java
+++ src/SudokuModel.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
 public class SudokuModel {
-
+	
     
     public static void main(String[] args){
 		System.out.println("sudoku");
 	}
+   private int[][] matrix = new int[9][9]; 
     
-    
+   
 	
 	
 	
