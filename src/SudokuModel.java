ca6f06823e9e99d8f75c1979958bd27c892c15e9
Gustav <none>
2018-02-15 16:26:33 +0100

    Added javadocs

--- src/SudokuModel.java
+++ src/SudokuModel.java
@@ -22,13 +22,14 @@ public class SudokuModel {
 			throw new IllegalArgumentException("Value needs to be an integer between 1 and 9");
 		}
 	}
-/**
- * Return the value at the destined position in the matrix, or 0 if a 
- * value has yet to be assigned.
- * @param row
- * @param column
- * @return
- */
+
+	/**
+	 * Return the value at the destined position in the matrix, or 0 if a 
+	 * value has yet to be assigned.
+	 * @param row
+	 * @param column
+	 * @return
+	 */
 	public int getValue(int row, int column) {
 		return nbrsMatrix[row][column];
 	}
