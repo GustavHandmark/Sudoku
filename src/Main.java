58403e3846b1fae0fe36fb6614a69fa175e4dfe5
Gustav <none>
2018-02-20 16:49:38 +0100

    Implemented the recursive method, todo: documentation

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,56 @@ public class Main {
 	// Currently used for manual testing
 	public static void main(String[] args) {
 		SudokuModel sm = new SudokuModel();
-		sm.setValue(3, 1, 1);
-		sm.setValue(5, 8, 8);
-		sm.setValue(9, 7, 8);
-		sm.setValue(4, 3, 5);
-		sm.setValue(8, 6, 3);
-		sm.setValue(2, 2, 5);
+//		sm.setValue(3, 1, 1);
+//		sm.setValue(5, 8, 8);
+//		sm.setValue(9, 7, 8);
+//		sm.setValue(4, 3, 5);
+//		sm.setValue(8, 6, 3);
+//		sm.setValue(2, 2, 5);
+//		sm.setValue(5, 1, 8);
+
+		sm.setValue(8, 0, 2);
+		sm.setValue(1, 2, 0);
+		sm.setValue(2, 2, 2);
+
+		sm.setValue(9, 0, 5);
+		sm.setValue(5, 2, 3);
+		
+		sm.setValue(6, 0, 7);
+		sm.setValue(2, 0, 8);
 		sm.setValue(5, 1, 8);
 
+		sm.setValue(5, 4, 1);
+		sm.setValue(6, 5, 0);
+
+		sm.setValue(2, 3, 3);
+		sm.setValue(1, 3, 4);
 		
+		sm.setValue(9, 3, 7);
+		sm.setValue(6, 4, 6);
+		sm.setValue(2, 5, 7);
+		sm.setValue(8, 5, 8);
+
+		sm.setValue(4, 6, 0);
+		sm.setValue(1, 6, 1);
+		sm.setValue(8, 7, 0);
+		sm.setValue(6, 7, 1);
+		
+		sm.setValue(6, 6, 3);
+		sm.setValue(8, 6, 5);
+		sm.setValue(3, 7, 4);
+		
+		sm.setValue(1, 7, 6);
+		sm.setValue(4, 8, 6);
+
+
+
+
 		// testar reglerna
-		sm.setValue(4, 3, 2);
-		sm.setValue(5, 8, 5);
-		sm.setValue(4, 4, 5);
-		System.out.println(sm.checkRules(8, 7, 5));
-
-		System.out.println(sm.getValue(8, 8));
+//		System.out.println(sm.checkRules(8, 7, 5));
 		sm.printMatrix();
-
+		sm.solveSudoku();
+		
+		sm.printMatrix();
 	}
 }
--- src/SudokuModel.java
+++ src/SudokuModel.java
@@ -10,6 +10,14 @@ public class SudokuModel {
 	public SudokuModel() {
 		nbrsMatrix = new int[9][9];
 	}
+	
+	public SudokuModel(int[][] preexisting){
+		//Checker for importing a preexisting matrix, should match the criterias given by checkrules:
+		// so for each value in the imported matrix, call checkrules
+		// should be a 9 by 9 matrix, else throw illegalargumentexception
+		
+	}
+	
 
 	/**
 	 * Sets the given value at the given position in the matrix
@@ -34,51 +42,10 @@ public class SudokuModel {
 		return nbrsMatrix;
 	}
 
-	/**
-	 * Return true if the placement of a value is acceptable, else false. RULES
-	 * 1. talet måste vara mellan 1 och 9. 2. Only one occurance of a number per
-	 * row || column 3. Only one occurance of a number within a region Return
-	 * true if the placement of a value is acceptable, else false. RULES 1.
-	 * talet måste vara mellan 1 och 9. 2. Only one occurance of a number per
-	 * row || column 3. Only one occurance of a number within a region
-	 * 
-	 * @param value
-	 * @param row
-	 * @param column
-	 * @return
-	 */
-	// public boolean checkRules(int value, int row, int column) {
-	// if (value > 0 && value < 10) {
-	// for (int i = 0; i < 9; i++) {
-	// if (matrix[row][i] != null || matrix[i][column] != null) {
-	// if (i != column && matrix[row][i].value == value) { // check row
-	// System.out.println("Detta värde finns redan på denna rad på plats: " +
-	// matrix[row][i].value + "[" + row + "," + i + "]");
-	// return false;
-	// } else if (i != row && matrix[i][column].value == value) {// check column
-	// System.out.println("Detta värde finns redan i denna kolumn på plats: " +
-	// matrix[i][column].value + "[" + i + "," + column + "]");
-	// return false;
-	// } else if (value != 0 && regionList[getRegion(row, column)][i].value ==
-	// value) {// INTE KLAR
-	// System.out.println(
-	// "Detta värde finns redan i denna region på plats: " +
-	// regionList[getRegion(row, column)][i].value + "[" + getRegion(row,
-	// column) + "," + column + "]");
-	// return false;
-	// }
-	// }
-	// }
-	//
-	// } else {
-	// System.out.print("Value needs to be an integer between 1 and 9");
-	// return false;
-	// }
-	//
-	// return true;
-	// }
+	// todo -> documentation for method, same as below.
 	public boolean solveSudoku() {
 		return solveSudoku(0, 0);
+		
 	}
 
 	private boolean solveSudoku(int row, int col) {
@@ -94,19 +61,50 @@ public class SudokuModel {
 		 * if no value (1-9) is a possible candidate, go back to the previous
 		 * cell and try another possible candidate then repeat.
 		 * 
-		 * If the sudoku has no solution, i.e.the last cannot fulfill the
-		 * condition so as that it is cleared -> return false
+		 * Where the sudoku has no solution, it is by error of the user (preexisting matrix or assigned values),
+		 * hence this is checked before the method call for this recursive method.
 		 */
-		if (row == 8 && col == 8 && checkRules(8, 9, nbrsMatrix[9][9]) == true) {
-			return true;
-		}
 
-		// implement recursive solver for the sudoku.
+		// Non-empty cell -> proceed to the next row-col.
+		
+		if (nbrsMatrix[row][col] > 0) {
+			if (col == 8) {
+				return solveSudoku(row + 1, 0);
+			} else {
+				return solveSudoku(row, col + 1);
+			}
+
+		}
+		// Empty cells, Recursively attemps to solve the sudoku. 
+		for (int value = 1; value <= 9; value++) {
+			if(row == 8 && col == 8 && checkRules(row,col,value)){ //Basecase - if we're at the last cell and the candidate number is possible -> sudoku is solved.
+				nbrsMatrix[row][col] = value;
+				return true;
+			}
+			if (checkRules(row, col, value)) {
+				nbrsMatrix[row][col] = value;
+				if (col == 8) {
+					if (solveSudoku(row + 1, 0)) {
+						return true; 
+					}
+
+				} else {
+					if (solveSudoku(row, col + 1)) {
+						return true;
+					}
+
+				}
+
+			}
+		}
+		nbrsMatrix[row][col] = 0; //No candidate is possible, set to 0, return false and go back.
 		return false;
 	}
 
+
+
 	/**
-	 * returns false if the given value is already found within the sudoku.
+	 * returns false if the given value interferes with any of the sudoku rules.
 	 * returns true if the given value is a possible candidate.
 	 * 
 	 * @param row
@@ -115,31 +113,33 @@ public class SudokuModel {
 	 * @return
 	 */
 	public boolean checkRules(int row, int col, int value) {
-		// Checks if the parameter value is permitted depending on the numbers
-		// in the given row
-		// the parameter value should not return false.
+		/* Checks if the parameter value is permitted depending on the numbers
+		 * in the given row.
+		 * Should not return false when the parameter value is checked against itself
+		 */ 
 		for (int i = 0; i < 9; i++) {
 			if (nbrsMatrix[row][i] == value && i != col) {
 				return false;
 			}
 		}
 
-		// Checks if the value is permitted depending on the numbers in the
-		// given column,
-		// the parameter value should not return false.
+		/* Checks if the value is permitted depending on the numbers in the
+		 * given column,
+		 * the parameter value should not return false.
+		 */
 		for (int i = 0; i < 9; i++) {
 			if (nbrsMatrix[i][col] == value && i != row) {
 				return false;
 			}
 		}
 
-		// Checks if the value is permitted depending on the numbers in the
-		// matrix region.
+		/* Checks if the value is permitted depending on the numbers in the
+		 * matrix region.
+		 */
 		int regionRow = (row / 3) * 3;
 		int regionCol = (col / 3) * 3;
-
 		for (int i = regionRow; i <= regionRow + 2; i++) {
-			for (int j = regionCol; j <= regionCol + 2; i++) {
+			for (int j = regionCol; j <= regionCol + 2; j++) {
 				// The number which we're checking should not throw a false,
 				if (nbrsMatrix[i][j] == value && (regionRow + i) != row && (regionCol + j) != col) {
 					return false;
