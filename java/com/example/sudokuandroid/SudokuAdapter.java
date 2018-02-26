2d7e22c76c4c6ea5cbf1ef1065ff76bb24b3cbc1
chokladio <chokladio@gmail.com>
2018-02-26 11:33:00 +0100

    Numeriskt filter för EditText

--- java/com/example/sudokuandroid/SudokuAdapter.java
+++ java/com/example/sudokuandroid/SudokuAdapter.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.IdRes;
 import android.text.InputType;
 import android.text.Layout;
+import android.text.Spanned;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.text.InputFilter;
 
 
 /**
@@ -33,6 +35,7 @@ public class SudokuAdapter extends BaseAdapter {
 
     }
 
+
     public void updateEditTextMatrix() {
         for (int i = 0; i < sm.getMatrix().length; i++) {
             for (int j = 0; j < sm.getMatrix()[i].length; j++) {
@@ -40,18 +43,50 @@ public class SudokuAdapter extends BaseAdapter {
                 box.setBackgroundResource(R.drawable.rectangle);
                 box.setInputType(InputType.TYPE_CLASS_NUMBER);
                 box.setGravity(Gravity.CENTER);
+
+                box.setFilters(new InputFilter[]{new InputFilterMinMax(1, 9)});
+
                 String s = String.valueOf(sm.getMatrix()[i][j]);
                 box.setText(String.valueOf(sm.getMatrix()[i][j]));
-                if(s.equalsIgnoreCase("0")){
+                if (s.equalsIgnoreCase("0")) {
                     box.setText("");
-                } else{
+                } else {
                     box.setText(String.valueOf(sm.getMatrix()[i][j]));
                 }
                 sudokuMatrix[i][j] = box;
             }
         }
-
     }
+
+    /**
+     * Implements a "filter" that only allows values between 1 and 9. Called every time a EditText-box is updated
+     */
+    public class InputFilterMinMax implements InputFilter {
+        private int min;
+        private int max;
+
+        public InputFilterMinMax(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            //noinspection EmptyCatchBlock
+            try {
+                int input = Integer.parseInt(dest.subSequence(0, dstart).toString() + source + dest.subSequence(dend, dest.length()));
+                if (isInRange(min, max, input))
+                    return null;
+            } catch (NumberFormatException nfe) {
+            }
+            return "";
+        }
+
+        private boolean isInRange(int a, int b, int c) {
+            return b > a ? c >= a && c <= b : c >= b && c <= a;
+        }
+    }
+
 
     /**
      * Updates the sudokuMatrix, returns true if successful,
@@ -61,7 +96,7 @@ public class SudokuAdapter extends BaseAdapter {
      */
 
 
-    public SudokuModel getSudoku(){
+    public SudokuModel getSudoku() {
         return sm;
     }
 
@@ -73,7 +108,7 @@ public class SudokuAdapter extends BaseAdapter {
                     try {
                         sm.setValue(Integer.parseInt(s), i, j);
                     } catch (IllegalArgumentException t) {
-                        sm.clearValue(i,j);
+                        sm.clearValue(i, j);
                     }
 
                 }
@@ -87,10 +122,10 @@ public class SudokuAdapter extends BaseAdapter {
     public void solveSudoku() {
         updateSudoku();
         Boolean check = sm.solveSudoku();
-        if(check == false){
+        if (check == false) {
             System.out.println("Användarinmatningsfel");
         }
-        if(check == true){
+        if (check == true) {
             System.out.println("Funkade");
         }
 
@@ -118,9 +153,9 @@ public class SudokuAdapter extends BaseAdapter {
     }
 
     public Object getItem(int position) {
-        int row=position/9;
-        int col=position%9;
-        if(position<=80){
+        int row = position / 9;
+        int col = position % 9;
+        if (position <= 80) {
             return sudokuMatrix[row][col];
         }
         return null;
@@ -132,33 +167,34 @@ public class SudokuAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         EditText et;
-        if(convertView == null){
+        if (convertView == null) {
             et = new EditText(mContext);
 
             int row;
             int col;
 
             //There must be a better way to do this, fml
-            if(position <=80) {
+            if (position <= 80) {
                 row = position / 9;
                 col = position % 9;
                 et = sudokuMatrix[row][col];
-                if(position<3){
+                if (position < 3) {
                     et.setBackgroundResource(R.drawable.rectangle_dark);
 
                 }
 
-                if(position>8 && position <12){
+                if (position > 8 && position < 12) {
                     et.setBackgroundResource(R.drawable.rectangle_dark);
                 }
-                if(position>17 && position <21){
+                if (position > 17 && position < 21) {
                     et.setBackgroundResource(R.drawable.rectangle_dark);
                 }
                 return et;
             }
         } else {
             et = (EditText) convertView;
-        };
+        }
+        ;
         return et;
 
 
